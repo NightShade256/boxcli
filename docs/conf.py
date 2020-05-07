db1be2652eec1a0f9a9adb09316a8ed35a34259d
Anish Jewalikar <anishjewalikar@gmail.com>
2020-05-07 16:52:23 +0530

    Maybe fix readthedocs?

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ with open('../boxcli/__init__.py') as f:
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
 
-
+master_doc = 'index'
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
